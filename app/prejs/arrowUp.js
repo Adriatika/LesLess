fdadeb03c0641a994dbe869e1813b54bf3ac382d
Adriatika <AdriatikaRich@gmail.com>
2018-12-07 10:02:59 +0700

    Смещение вверх дополнительной стрелки прокрутки.

--- app/prejs/arrowUp.js
+++ app/prejs/arrowUp.js
@@ -1,15 +1,22 @@
-$(document).ready(function(){
-  $(".main__upArrow").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top}, 800, function(){
-        window.location.hash = hash;
-      });
-    } 
-  });
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
 });
+// $(document).ready(function(){
+//   $(".main__upArrow").on('click', function(event) {
+//     if (this.hash !== "") {
+//       event.preventDefault();
+//       var hash = this.hash;
+//       $('html, body').animate({
+//         scrollTop: $(hash).offset().top}, 800, function(){
+//         window.location.hash = hash;
+//       });
+//     } 
+//   });
+// });
 jQuery(function($){
   $(window).scroll(function(){
     // $('.main__upArrowDouble').css('display', 'block');
