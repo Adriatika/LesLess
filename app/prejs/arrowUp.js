9e79419fbcdf37588aba95e533dc98ccf08cf876
Adriatika <AdriatikaRich@gmail.com>
2018-12-07 15:00:15 +0700

    стрелка вверх

--- app/prejs/arrowUp.js
+++ app/prejs/arrowUp.js
@@ -5,23 +5,11 @@ $('a[href*="#"]').click(function() {
     }, 400);
     return false;
 });
-// $(document).ready(function(){
-//   $(".main__upArrow").on('click', function(event) {
-//     if (this.hash !== "") {
-//       event.preventDefault();
-//       var hash = this.hash;
-//       $('html, body').animate({
-//         scrollTop: $(hash).offset().top}, 800, function(){
-//         window.location.hash = hash;
-//       });
-//     } 
-//   });
-// });
+
 jQuery(function($){
   $(window).scroll(function(){
-    // $('.main__upArrowDouble').css('display', 'block');
     var bottomOffset = $(window).height();
-    if($(document).scrollTop() > (0.5*bottomOffset)){
+    if($(document).scrollTop() > (0.2*bottomOffset)){
       $('.main__upArrowDouble').addClass('arrowPosition').css('display', 'block');
     }else if($(document).scrollTop() < (0.5*bottomOffset)){
       $('.main__upArrowDouble').removeClass('arrowPosition').css('display', 'none');}
