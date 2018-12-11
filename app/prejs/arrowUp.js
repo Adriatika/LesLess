6eb1572fb6a68da7935b3f340b3ba16a5d3d251f
Adriatika <AdriatikaRich@gmail.com>
2018-12-11 14:33:14 +0700

    Убрана css-анимация со стрелки подъёма, исправлен скрипт появления стрелки.

--- app/prejs/arrowUp.js
+++ app/prejs/arrowUp.js
@@ -5,15 +5,28 @@ $('a[href*="#"]').click(function() {
     }, 400);
     return false;
 });
-
-jQuery(function($){
-  $(window).scroll(function(){
-    var bottomOffset = $(window).height();
-    if($(document).scrollTop() > (0.2*bottomOffset)){
-      $('.main__upArrowDouble').addClass('arrowPosition').css('display', 'block');
-    }else if($(document).scrollTop() < (0.5*bottomOffset)){
-      $('.main__upArrowDouble').removeClass('arrowPosition').css('display', 'none');}
-      if(($(document).scrollTop() + bottomOffset+300) > ($(document).height())){
-        $('.main__upArrowDouble').removeClass('arrowPosition').css('display', 'none');}
-      });
-});
+$(window).scroll(function(){
+  var clfx = $(".wrapper").height()-1450;
+  console.log("height = "+ clfx);
+  console.log($(window).scrollTop());
+  if($(window).scrollTop()>300 && $(window).scrollTop()<clfx){
+      $(".main__upArrowDouble").fadeIn();
+    }
+    if($(window).scrollTop()>=clfx){
+      $(".main__upArrowDouble").fadeOut();  
+    }
+    if($(window).scrollTop()<=300){
+      $(".main__upArrowDouble").fadeOut();    
+    }
+})
+// jQuery(function($){
+//   $(window).scroll(function(){
+//     var bottomOffset = $(window).height();
+//     if($(document).scrollTop() > (0.2*bottomOffset)){
+//       $('.main__upArrowDouble').addClass('arrowPosition').css('display', 'block');
+//     }else if($(document).scrollTop() < (0.5*bottomOffset)){
+//       $('.main__upArrowDouble').removeClass('arrowPosition').css('display', 'none');}
+//       if(($(document).scrollTop() + bottomOffset+300) > ($(document).height())){
+//         $('.main__upArrowDouble').removeClass('arrowPosition').css('display', 'none');}
+//       });
+// });
